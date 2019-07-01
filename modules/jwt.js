2b0fb1e17cd327dd514a437f439245fc18759e0f
SERIN <aloha_rini@naver.com>
2019-07-01 22:54:48 +0900

    add config at ignore

---
+++ modules/jwt.js
@@ -0,0 +1,59 @@
+var randtoken = require('rand-token');
+const jwt = require('jsonwebtoken');
+const secretOrPrivateKey = "jwtSecretKey!";
+const options = {
+    algorithm: "HS256",
+    expiresIn: "1h",
+    issuer: "genie"
+};
+const refreshOptions = {
+    algorithm: "HS256",
+    expiresIn: "24h * 14",
+    issuer: "genie"
+};
+
+module.exports = {
+    sign: (user) => {
+        const payload = {
+            idx: user.idx,
+            grade: user.grade,
+            name: user.name
+        };
+
+        const result = {
+            token: jwt.sign(payload, secretOrPrivateKey, options),
+            // refreshToken: randtoken.uid(256)
+            // ^ option
+        };
+        //refreshToken을 만들 때에도 다른 키를 쓰는게 좋다.
+
+        return result;
+    },
+    verify: (token) => {
+        let decoded;
+        try {
+            decoded = jwt.verify(token, secretOrPrivateKey);
+        } catch (err) {
+            if (err.message === 'jwt expired') {
+                console.log('expired token');
+                return -3;
+            } else if (err.message === 'invalid token') {
+                console.log('invalid token');
+                return -2;
+            } else {
+                console.log("invalid token");
+                return -2;
+            }
+        }
+        return decoded;
+    },
+    refresh: (user) => {
+        const payload = {
+            idx: user.idx,
+            grade: user.grade,
+            name: user.name
+        };
+
+        return jwt.sign(payload, secretOrPrivateKey, options);
+    }
+};
