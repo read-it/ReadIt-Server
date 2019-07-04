5ec45a0f2facb9daa68686ad9b36969474993f10
Gobukgol <zxczc159@gmail.com>
2019-07-04 12:38:16 +0900

    git pull 전 commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authMiddleware = require('../modules/utils/authUtils')
 // router.use('/user', require('./user/index'));
 // router.use('/category', require('./category/index'));
-// router.use('/contents', require('./contents/index'));
+router.use('/contents', require('./contents/index'));
 // router.use('/mypage', require('./mypage/index'));
 router.use('/storage', require('./storage/index'))
 
