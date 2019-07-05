c6f4f08590346c5d75f71cd7e4ab1d91ddd728dc
Gobukgol <zxczc159@gmail.com>
2019-07-05 17:32:41 +0900

    1.콘텐츠 스크랩 구현

--- module/utils/responseMessage.js
+++ module/utils/responseMessage.js
@@ -8,6 +8,8 @@ module.exports = {
     DELETE_CONTENTS_SUCCESS: "콘텐츠 삭제 성공",
     READ_CONTENTS_SUCCESS: "콘텐츠 읽음 처리 성공",
     ADD_CONTENTS_SUCCESS: "콘텐츠 추가 성공",
+    SCRAP_DELETE_SUCCESS: "콘텐츠 스크랩 제거 성공",
+    SCRAP_SUCCESS: "콘텐츠 스크랩 성공",
     NO_CONTENT: "게시글이 존재하지 않습니다",
 
     INVALID_TOKEN: "잘못된 형식의 토큰입니다.",
--- routes/contents/index.js
+++ routes/contents/index.js
@@ -4,5 +4,6 @@ const router = express.Router()
 router.use('/add',require('./add'))
 router.use('/change',require('./change'))
 router.use('/delete',require('./delete'))
+router.use('/scrap',require('./scrap'))
 
 module.exports = router
