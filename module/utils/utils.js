36228d21bd6a49cca33ee93291dc1845d75bd911
Gobukgol <zxczc159@gmail.com>
2019-07-10 02:54:04 +0900

    콘텐츠 리스트에 등록시간 추가

--- module/utils/utils.js
+++ module/utils/utils.js
@@ -1,3 +1,4 @@
+const moment = require('moment')
 const util = {
     successTrue: (status, message, data) => {
         return {
@@ -13,6 +14,30 @@ const util = {
             success: false,
             message: message
         }
+    },
+    getAfterCreateDate: (date) => {
+        var createDate = moment(date)
+        console.log(createDate)
+        var currentDate = moment()
+        console.log(currentDate)
+
+        var diff = currentDate.diff(createDate,"day")
+
+        if(diff == 0){
+            return '오늘'
+        } else if(diff < 8){
+            return diff.toString().concat('일전')
+        } else if(currentDate.diff(createDate,'year') == 0){
+            return createDate.format('M월 DD일')
+        } else {
+            return createDate.format('YYYY.MM.DD')
+        }
+    },
+    insertAfterCreateDateAtResult: (selectResult) => {
+        for(i = 0;i<selectResult.length;i++){
+            selectResult[i].afterCreateDate = util.getAfterCreateDate(selectResult[i].created_date)
+        }
+        return selectResult
     }
 };
 
