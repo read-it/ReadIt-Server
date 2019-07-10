1ad99cbb409aec8d17268659a0b7552da313cdfc
Gobukgol <zxczc159@gmail.com>
2019-07-10 18:13:00 +0900

    콘텐츠 삭제 오타수정 마지막...

--- routes/contents/delete.js
+++ routes/contents/delete.js
@@ -32,7 +32,7 @@ router.put('/:contents_idx',authUtils.isLoggedin,async(req, res) => {
     var updateResult = await db.queryParam_Arr(updateContentsQuery,[req.params.contents_idx])
 
     if(updateResult == null){
-        res.status(200).send(util.successFalse(statusCodeDB_ERROR,resMessage.DB_ERROR))
+        res.status(200).send(util.successFalse(statusCode.DB_ERROR,resMessage.DB_ERROR))
     } else if(updateResult.affectedRows != 1){
         res.status(200).send(util.successFalse(statusCode.BAD_REQUEST,resMessage.OUT_OF_VALUE))
     } else {
